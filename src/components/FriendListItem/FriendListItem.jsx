5ffdcbbf1153676525ea37d34ca6f3db305a17c7
Yurii Kit <insidelkit@gmail.com>
2025-04-20 10:39:01 +0300

    added clsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,19 @@
 import css from './FriendListItem.module.css';
+import clsx from 'clsx';
 
 const FriendListItem = ({ name, avatar, isOnline }) => {
-  const classNames = [css.text];
-  classNames.push(isOnline ? css.online : css.offline);
+  // const classNames = [css.text];
+  // classNames.push(isOnline ? css.online : css.offline);
+  const finalCls = clsx(css.text, {
+    [css.online]: isOnline,
+    [css.offline]: !isOnline,
+  });
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p className={css.text}>{name}</p>
-      <p className={classNames.join(' ')}>{isOnline ? 'Online' : 'Offline'}</p>
+      <p className={finalCls}>{isOnline ? 'Online' : 'Offline'}</p>
+      {/* <p className={classNames.join(' ')}>{isOnline ? 'Online' : 'Offline'}</p> */}
     </div>
   );
 };
